79deb653bea6f15a9b17200013c292f1bd0f77ec
tb38r <tlawal86@hotmail.co.uk>
2023-04-13 23:49:19 +0100

    jest testing

--- tests/sum.js
+++ tests/sum.js
@@ -1,3 +1,8 @@
-const sum = (a, b)=> a +b
+const sum = (a, b)=> {
+ if (isNaN(a)|| isNaN(b)) {
+   return undefined
+ }
+    return a +b
+}
 
 module.exports = sum
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,8 +1,19 @@
 const sum = require('./sum')
 
+describe("Suite for Sum", ()=>{ 
+    
 test('properly adds two numbers', ()=>{
     expect(
         sum(1,2)
         ).toBe(3)
+
+})
+
+test('only accepts integers', ()=>{
+    expect(
+        sum('A',2)
+        ).toBe(undefined)
+
+})
 
 })
