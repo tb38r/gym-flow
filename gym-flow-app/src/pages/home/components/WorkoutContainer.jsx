e427d9c03718111f0488c229af9ecd1efd673d75
tb38r <tlawal86@hotmail.co.uk>
2023-04-21 18:04:03 +0100

    added half the workout modals

--- gym-flow-app/src/pages/home/components/WorkoutContainer.jsx
+++ gym-flow-app/src/pages/home/components/WorkoutContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@mui/material';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
@@ -19,14 +19,26 @@ const style = {
 };
 
 const WorkoutContainer = (props) => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [legOpen, setLegOpen] = React.useState(false);
+  const handleLegOpen = () => setLegOpen(true);
+  const handleLegClose = () => setLegOpen(false);
+
+  const [chestOpen, setChestOpen] = React.useState(false);
+  const handleChestOpen = () => setChestOpen(true);
+  const handleChestClose = () => setChestOpen(false);
 
   const HandleClick = (e) => {
     e.preventDefault();
-    handleOpen();
-    console.log(e.target.id);
+    let clickedOption = e.target.id;
+
+    switch (clickedOption) {
+      case 'legs':
+        handleLegOpen();
+        break;
+      case 'chest-triceps':
+        handleChestOpen();
+        break;
+    }
   };
 
   return (
@@ -41,8 +53,8 @@ const WorkoutContainer = (props) => {
         </div>
       </div>
       <Modal
-        open={open}
-        onClose={handleClose}
+        open={legOpen}
+        onClose={handleLegClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description">
         <Box sx={style}>
@@ -50,7 +62,22 @@ const WorkoutContainer = (props) => {
             Text in a modal
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            Leg stuff
+          </Typography>
+        </Box>
+      </Modal>
+
+      <Modal
+        open={chestOpen}
+        onClose={handleChestClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Chest Stuff{' '}
           </Typography>
         </Box>
       </Modal>
